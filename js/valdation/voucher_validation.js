c878f8b1342b580632cd42d6e4f2ad491694aecb
phamminh644 <82488492+phamminh644@users.noreply.github.com>
2022-04-03 10:20:11 +0700

    voucher update

--- js/valdation/voucher_validation.js
+++ js/valdation/voucher_validation.js
@@ -2,23 +2,6 @@ let x = 0;
 const form = document.getElementById('form');
 const price = document.getElementById('order-deposit');
 
-//Xét điều kiện để hiện thông báo lỗi form add
-const setError = (element, message) => {
-    const inputControl = element.parentNode;
-    const errorDisplay = inputControl.querySelector('.fail');
-    errorDisplay.innerText = message;
-    inputControl.classList.add('failed');
-    inputControl.classList.remove('success')
-}
-//Xét điều kiện để hiện thông báo thành công form add
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.fail');
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('failed');
-    x = x + 1;
-};
 
 //Chạy hàm validate form add
 form.addEventListener('submit', e => {
