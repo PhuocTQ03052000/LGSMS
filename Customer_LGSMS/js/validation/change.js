1223a635ef2a3f1eafae5181a4983858f1e5b0d7
phamminh644 <82488492+phamminh644@users.noreply.github.com>
2022-04-06 14:50:25 +0700

    customer validation

---
+++ Customer_LGSMS/js/validation/change.js
@@ -0,0 +1,49 @@
+let x = 0;
+const form = document.getElementById('form');
+const oldpass = document.getElementById('old-pass');
+const newpass = document.getElementById('new-pass');
+const repass = document.getElementById('re-pass');
+
+//Chạy hàm validate form add
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    x = 0;
+    validateInputs();
+    if (x == 3) {
+        // document.getElementById("form").submit();
+        alertFunction();
+        //form đã được submit tôi chưa biết so sánh form với database tn nên bạn thử sau đây noti trong if hoạt động oke
+        
+    }
+});
+
+//Trim input và các thông báo
+const validateInputs = () => {
+    const old_passValue = oldpass.value.trim();
+    const new_passValue = newpass.value.trim();
+    const re_passValue = repass.value.trim();
+
+    //Thông báo nhập mật khẩu
+    if (new_passValue === '') {
+        setError(newpass, 'Yêu cầu nhập mật khẩu');
+    } else if (new_passValue.length < 8) {
+        setError(newpass, 'Mật khẩu tối thiểu 8 kí tự.')
+    } else {
+        setSuccess(newpass);
+    }
+    //Thông báo xác nhận mật khảu
+    if (re_passValue === '') {
+        setError(repass, 'Yêu cầu xác nhận mật khẩu');
+    } else if (re_passValue !== new_passValue) {
+        setError(repass, "Không trùng với mật khẩu");
+    } else {
+        setSuccess(repass);
+    }
+
+    //Thông báo mật khẩu
+    if (old_passValue === '') {
+        setError(oldpass, 'Yêu cầu nhập tên tài khoản');
+    } else {
+        setSuccess(oldpass);
+    }
+};
